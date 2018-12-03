c5e834a06fa91ca5ba7a9cef289536bf4bb9b68a
Matt Shwery <mshwery@gmail.com>
2018-12-02 20:06:51 -0600

    test circle

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ const config = require('./config')
 
 module.exports = {
   type: 'postgres',
-  host: config.get('PGHOST'),
-  port: Number(config.get('PGPORT')),
+  host: config.get('PGHOST') || 'localhost',
+  port: Number(config.get('PGPORT')) || 5432,
   database: config.get('PGDATABASE'),
-  username: config.get('PGUSER'),
-  password: config.get('PGPASSWORD'),
+  username: config.get('PGUSER') || 'postgres',
+  password: config.get('PGPASSWORD') || '',
   // output dir after compilation
   entities: ['dist/app/**/*.entity.js'],
   // output dir after compilation
