c5fb456f411d6f50a5a63b8888b42e61ce23a3cf
Matt Shwery <mshwery@gmail.com>
2019-01-06 15:06:40 -0600

    move root resolvers and typeDefs

--- app/graphql/resolvers.ts
+++ app/graphql/resolvers.ts
@@ -1,15 +1,8 @@
 import { merge } from 'lodash'
-import { GraphQLDate, GraphQLDateTime } from 'graphql-iso-date'
+import { resolvers as rootResolvers } from './root'
 import { resolvers as authResolvers } from './auth'
 import { resolvers as listResolvers } from './list'
 import { resolvers as taskResolvers } from './task'
 import { resolvers as userResolvers } from './user'
 
-const scalarResolvers = {
-  // Custom ISO 8601 date (no time) resolver
-  Date: GraphQLDate,
-  // Custom ISO 8601 UTC datetime
-  DateTime: GraphQLDateTime
-}
-
-export default merge(scalarResolvers, authResolvers, listResolvers, taskResolvers, userResolvers)
+export default merge(rootResolvers, authResolvers, listResolvers, taskResolvers, userResolvers)
--- app/graphql/typeDefs.ts
+++ app/graphql/typeDefs.ts
@@ -1,30 +1,7 @@
-import { gql } from 'apollo-server-express'
+import { typeDef as RootSchema } from './root'
 import { typeDef as Auth } from './auth'
 import { typeDef as List } from './list'
 import { typeDef as Task } from './task'
 import { typeDef as User } from './user'
 
-const schema = gql`
-  schema {
-    query: Query
-    mutation: Mutation
-  }
-
-  # Root Query and Mutation defined so they can be extended in other schema parts.
-  type Query
-  type Mutation
-
-  # An ISO-8601 UTC datetime.
-  scalar DateTime
-
-  # An ISO-8601 date (without time).
-  scalar Date
-
-  # An object with an ID.
-  interface Node {
-    # ID of the object.
-    id: ID!
-  }
-`
-
-export default [schema, Auth, List, Task, User]
+export default [RootSchema, Auth, List, Task, User]
