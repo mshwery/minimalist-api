b5135358fa39242fbaa2899560acad8557c40253
Matt Shwery <mshwery@gmail.com>
2019-04-20 17:24:48 -0500

    add user to Request type

--- app/index.d.ts
+++ app/index.d.ts
@@ -2,4 +2,12 @@ declare namespace Express {
   interface Response {
     sentry: string
   }
+
+  interface Request {
+    // The currently authenticated user object
+    user?: {
+      // The jwt "subject" (aka user id)
+      sub: string
+    }
+  }
 }
