b977713cbfa0db74639188bd8957d39b6d0cf841
Matt Shwery <matthew.shwery@segment.com>
2019-12-14 12:35:52 -0600

    add due date column (#66)

---
+++ migrations/1575482104279-AddDueDateToTasks.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm'
+
+const dueColumn = new TableColumn({
+  name: 'due',
+  type: 'timestamp with time zone',
+  isNullable: true
+})
+
+export class AddDueDateToTasks1575482104279 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.addColumn('tasks', dueColumn)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropColumn('tasks', dueColumn)
+  }
+}
